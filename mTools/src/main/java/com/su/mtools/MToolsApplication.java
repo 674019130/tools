258c6f2377c4d5cf427d231b43525073a6426c24
Xiang cong Meng <v-xiameng@microsoft.com>
2021-08-10 17:04:06 +0800

    chore: SpringBoot init

---
+++ mTools/src/main/java/com/su/mtools/MToolsApplication.java
@@ -0,0 +1,13 @@
+package com.su.mtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MToolsApplication.class, args);
+    }
+
+}
---
+++ mTools/src/test/java/com/su/mtools/MToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.su.mtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
