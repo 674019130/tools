821f7d112cdc011a51be527fddfb833d90308d93
Xiang cong Meng <v-xiameng@microsoft.com>
2021-08-12 17:59:59 +0800

    chore: delete useless code

--- mTools/src/main/java/com/su/mtools/MToolsApplication.java
+++ mTools/src/main/java/com/su/mtools/MToolsApplication.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 @MapperScan(basePackages = {"com.su.mtools.mvc.mapper"})
 public class MToolsApplication {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         SpringApplication.run(MToolsApplication.class, args);
     }
 
