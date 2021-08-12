619e11569db17b73bcde0bb0a6cb5797e63c467e
Xiang cong Meng <v-xiameng@microsoft.com>
2021-08-12 17:59:39 +0800

    chore: add springbootApplication annotations

--- mTools/src/main/java/com/su/mtools/MToolsApplication.java
+++ mTools/src/main/java/com/su/mtools/MToolsApplication.java
@@ -1,12 +1,18 @@
 package com.su.mtools;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan(basePackages = {"com.su.mtools.mvc.mapper"})
 public class MToolsApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         SpringApplication.run(MToolsApplication.class, args);
     }
 
